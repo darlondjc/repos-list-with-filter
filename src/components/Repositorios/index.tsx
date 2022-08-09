264d325cf1841a37ed6da7e567a047e163ec962e
Darlon <darlondjc@gmail.com>
2022-08-09 13:21:18 -0300

    Refatoração de hooks

--- src/components/Repositorios/index.tsx
+++ src/components/Repositorios/index.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { RepoList } from "../RepoList";
-import { SearchBar } from "../SearchBar";
 
 export type Repository = {
     name: string;
@@ -10,66 +9,70 @@ export type Repository = {
     html_url: string;
 };
 
-export type StateData = {
-    onlyArchivedRepos: boolean,
-    termoPesquisa: string,
-    isFetching: boolean;
-    repositories: Repository[] | null
-};
-
 const urlGitHub = 'https://api.github.com/users/darlondjc/repos';
 
 export function Repositorios() {
-    const [repositories, setRepositories] = useState<Repository[] | null>(null);
-    const [isFetching, setIsFetching] = useState(true);
-    const [onlyArchivedRepos, setOnlyArchivedRepos] = useState(false);
-    const [termoPesquisa, setTermoPesquisa] = useState('');
+    const onlyArchivedRepos = useFormInput(false, 'b');
+    const termoPesquisa = useFormInput('', 's');
 
-    /*assim que a tela for carregada*/
-    useEffect(() => {
-        useFetch(urlGitHub);
-    }, []);
-
-    function useFetch(url: string) {
-        axios.get(url)
-            .then(response => setRepositories(response.data))
-            .finally(() => setIsFetching(false));
-    }
-
-    function handleChangeTexto(event: any) {
-        setTermoPesquisa(event.target.value);
-    }
-
-    function handleChangeCheckbox(event: any) {
-        setOnlyArchivedRepos(event.target.checked);
-    }
-
-    const listaFilteredRepos = repositories?.filter(repo => {
-        if (onlyArchivedRepos) {
-            if (termoPesquisa != null) {
-                return repo.archived && repo.name.includes(termoPesquisa);
+    const buscaDeRepositorios = buscaRepositorios();
+    const listaFilteredRepos = buscaDeRepositorios.repositories?.filter(repo => {
+        if (onlyArchivedRepos.value) {
+            if (termoPesquisa.value != null) {
+                return repo.archived && repo.name.includes(termoPesquisa.value);
             } else {
                 return repo.archived;
             }
         } else {
-            return repo.name.toLowerCase().includes(termoPesquisa.toLowerCase());
+            return repo.name.toLowerCase().includes(termoPesquisa.value.toLowerCase());
         }
     });
     //console.log(listaFilteredRepos);
 
     return (
         <div>
-            <SearchBar
-                privateRepos={onlyArchivedRepos}
-                termo={termoPesquisa}
-                onChangeTexto={handleChangeTexto}
-                onChangeCheckbox={handleChangeCheckbox}
-            />
-            {isFetching && <p>Carregando...</p>}
+            <form>
+                <input type="text" placeholder="Repo a ser pesquisado..." {...termoPesquisa} />
+                <p>
+                    <input type="checkbox" {...onlyArchivedRepos} /> {' '} Arquivados
+                </p>
+            </form>
+            {buscaDeRepositorios.isFetching && <p>Carregando...</p>}
             <RepoList lista={listaFilteredRepos} />
         </div>
     );
 
+}
+
+function buscaRepositorios() {
+    const [repositories, setRepositories] = useState<Repository[] | null>(null);
+    const [isFetching, setIsFetching] = useState(true);
+
+    /*assim que a tela for carregada*/
+    useEffect(() => {
+        axios.get(urlGitHub)
+            .then(response => setRepositories(response.data))
+            .finally(() => setIsFetching(false));
+    }, []);
+
+    return { repositories, isFetching };
+}
+
+function useFormInput(initialValue: any, type: string) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleChange(event: any) {
+        if (type === 's'){
+            setValue(event.target.value);
+        } else {
+            setValue(event.target.checked);
+        }
+    }
+
+    return {
+        value,
+        onChange: handleChange
+    }
 }
 
 export default Repositorios
